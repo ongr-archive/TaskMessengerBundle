70708540ca7520cacd72acaabfccb40e2c67d54c
Linas Mockus <linas.mockus@ongr.io>
2015-01-08 11:32:35 +0200

    Updated TaskPublisher funtional test to verify results.

--- Tests/Functional/Service/TaskPublisherTest.php
+++ Tests/Functional/Service/TaskPublisherTest.php
@@ -13,6 +13,7 @@ namespace ONGR\TaskMessengerBundle\Tests\Functional\Service;
 
 use ONGR\TaskMessengerBundle\Document\SyncTask;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Predis;
 
 class TaskPublisherTest extends WebTestCase
 {
@@ -22,11 +23,35 @@ class TaskPublisherTest extends WebTestCase
     public function testPublish()
     {
         $client = self::createClient();
-        $publisher = $client->getContainer()->get('ongr_task_messenger.task_publisher.default');
+        $publisher = $client->getContainer()->get('ongr_task_messenger.task_publisher.foo_publisher');
 
         $task = new SyncTask(SyncTask::SYNC_TASK_PRESERVEHOST);
         $task->setName('task_foo');
         $task->setCommand('command_foo');
         $publisher->publish($task);
+
+        $redis = new Predis\Client();
+        $this->verifyMessage($redis->get('test'));
+    }
+
+    /**
+     * Test if correct redis message is returned.
+     *
+     * @param string $message
+     */
+    public function verifyMessage($message)
+    {
+        $body = json_decode($message, true);
+
+        $this->assertEquals($body['task'], 'ongr.redis_task.task_foo');
+        $this->assertEquals($body['args'][0], 'command_foo -e test');
+    }
+
+    /**
+     * @return \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    public function getContainer()
+    {
+        return self::createClient()->getContainer();
     }
 }
